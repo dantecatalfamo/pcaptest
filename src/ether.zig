476fb8aec83d45c67b8ed109e99764ffad3b3ba2
dantecatalfamo <dante.catalfamo@gmail.com>
2023-06-11 21:12:05 -0400

    try doing capture

---
+++ src/ether.zig
@@ -0,0 +1,185 @@
+const mem = @import("std").mem;
+//IEEE 802.3 Ethernet magic constants. The frame sizes omit the
+//preamble and FCS/CRC (frame check sequence)
+
+// #define ETH_ALEN    6       /* Octets in one ethernet addr   */
+// #define ETH_TLEN    2       /* Octets in ethernet type field */
+// #define ETH_HLEN    14      /* Total octets in header.   */
+// #define ETH_ZLEN    60      /* Min. octets in frame sans FCS */
+// #define ETH_DATA_LEN    1500        /* Max. octets in payload    */
+// #define ETH_FRAME_LEN   1514        /* Max. octets in frame sans FCS */
+// #define ETH_FCS_LEN 4       /* Octets in the FCS         */
+
+// #define ETH_MIN_MTU 68      /* Min IPv4 MTU per RFC791  */
+// #define ETH_MAX_MTU 0xFFFFU     /* 65535, same as IP_MAX_MTU    */
+
+pub const Protocol = enum(u16) {
+    /// Ethernet Loopback packet
+    loop = 0x0060,
+    /// Xerox PUP packet
+    pup = 0x0200,
+    /// Xerox PUP Addr Trans packet
+    pupat = 0x0201,
+    /// TSN (IEEE 1722) packet
+    tsn = 0x22F0,
+    /// ERSPAN version 2 (type III)
+    erspan2 = 0x22EB,
+    /// Internet Protocol packet
+    ip = 0x0800,
+    /// CCITT X.25
+    x25 = 0x0805,
+    /// Address Resolution packet
+    arp = 0x0806,
+    /// G8BPQ AX.25 Ethernet Packet
+    bpq = 0x08FF,
+    /// Xerox IEEE802.3 PUP packet
+    ieeepup = 0x0a00,
+    /// Xerox IEEE802.3 PUP Addr Trans packet
+    ieeepupat = 0x0a01,
+    /// B.A.T.M.A.N.- Advanced packet
+    batman = 0x4305,
+    /// DEC Assigned proto
+    dec = 0x6000,
+    /// DEC DNA Dump/Load
+    dna_dl = 0x6001,
+    /// DEC DNA Remote Console
+    dna_rc = 0x6002,
+    /// DEC DNA Routing
+    dna_rt = 0x6003,
+    /// DEC LAT
+    lat = 0x6004,
+    /// DEC Diagnostics
+    diag = 0x6005,
+    /// DEC Customer use
+    cust = 0x6006,
+    /// DEC Systems Comms Arch
+    sca = 0x6007,
+    /// Trans Ether Bridging
+    teb = 0x6558,
+    /// Reverse Addr Res packet
+    rarp = 0x8035,
+    /// Appletalk DDP
+    atalk = 0x809B,
+    /// Appletalk AARP
+    aarp = 0x80F3,
+    /// 802.1Q VLAN Extended Header
+    @"8021q" = 0x8100,
+    /// ERSPAN type II
+    erspan = 0x88BE,
+    /// IPX over DIX
+    ipx = 0x8137,
+    /// IPv6 over bluebook
+    ipv6 = 0x86DD,
+    /// IEEE Pause frames. See 802.3 31B
+    pause = 0x8808,
+    /// Slow Protocol. See 802.3ad 43B
+    slow = 0x8809,
+    /// Web-cache coordination protocol defined in draft-wilson-wrec-wccp-v2-00.txt
+    wccp = 0x883E,
+    /// MPLS Unicast traffic
+    mpls_uc = 0x8847,
+    /// MPLS Multicast traffic
+    mpls_mc = 0x8848,
+    /// MultiProtocol Over ATM
+    atmmpoa = 0x884c,
+    /// PPPoE discovery messages
+    ppp_disc = 0x8863,
+    /// PPPoE session messages
+    ppp_ses = 0x8864,
+    /// HPNA, wlan link local tunnel
+    link_ctl = 0x886c,
+    /// Frame-based ATM Transport over Ethernet
+    atmfate = 0x8884,
+    /// Port Access Entity (IEEE 802.1X)
+    pae = 0x888E,
+    /// PROFINET
+    profinet = 0x8892,
+    /// Multiple proprietary protocols
+    realtek = 0x8899,
+    /// ATA over Ethernet
+    aoe = 0x88A2,
+    /// EtherCAT
+    ethercat = 0x88A4,
+    /// 802.1ad Service VLAN
+    @"8021ad" = 0x88A8,
+    ///  802.1 Local Experimental 1
+    @"802_ex1" = 0x88B5,
+    /// 802.11 Preauthentication
+    preauth = 0x88C7,
+    /// TIPC
+    tipc = 0x88CA,
+    /// Link Layer Discovery Protocol
+    lldp = 0x88CC,
+    /// Media Redundancy Protocol
+    mrp = 0x88E3,
+    /// 802.1ae MACsec
+    macsec = 0x88E5,
+    /// 802.1ah Backbone Service Tag
+    @"8021ah" = 0x88E7,
+    /// 802.1Q MVRP
+    mvrp = 0x88F5,
+    /// IEEE 1588 Timesync
+    @"1588" = 0x88F7,
+    /// NCSI protocol
+    ncsi = 0x88F8,
+    /// IEC 62439-3 PRP/HSRv0
+    prp = 0x88FB,
+    /// Connectivity Fault Management
+    cfm = 0x8902,
+    /// Fibre Channel over Ethernet
+    fcoe = 0x8906,
+    /// Infiniband over Ethernet
+    iboe = 0x8915,
+    /// TDLS
+    tdls = 0x890D,
+    /// FCoE Initialization Protocol
+    fip = 0x8914,
+    /// IEEE 802.21 Media Independent Handover Protocol
+    @"80221" = 0x8917,
+    /// IEC 62439-3 HSRv1
+    hsr = 0x892F,
+    /// Network Service Header
+    nsh = 0x894F,
+    /// Ethernet loopback packet, per IEEE 802.3
+    loopback = 0x9000,
+    /// deprecated QinQ VLAN
+    qinq1 = 0x9100,
+    /// deprecated QinQ VLAN
+    qinq2 = 0x9200,
+    /// deprecated QinQ VLAN
+    qinq3 = 0x9300,
+    /// Ethertype DSA
+    edsa = 0xDADA,
+    /// Fake VLAN Header for DSA
+    dsa_8021q = 0xDADB,
+    /// A5PSW Tag Value
+    dsa_a5psw = 0xE001,
+    /// ForCES inter-FE LFB type
+    ife = 0xED3E,
+    /// IBM af_iucv
+    af_iucv = 0xFBFB,
+    /// If the value in the ethernet type is more than this value * then the frame is Ethernet II. Else it is 802.3
+    @"802_3_min" = 0x0600,
+    /// Who knows
+    _,
+};
+
+pub const Header = struct {
+    /// Destunation ethernet address
+    dest: [6]u8,
+    /// Source ethernet address
+    source: [6]u8,
+    /// Packet type
+    proto: Protocol,
+
+    pub fn parse(bytes: []const u8) Header {
+        var header = Header{
+            .dest = undefined,
+            .source = undefined,
+            .proto = @intToEnum(Protocol, mem.readIntBig(u16, bytes[12..14])),
+        };
+        @memcpy(&header.dest, bytes[0..6]);
+        @memcpy(&header.source, bytes[6..12]);
+        return header;
+    }
+};
---
+++ src/main.zig
@@ -0,0 +1,99 @@
+const std = @import("std");
+const ether = @import("ether.zig");
+const c = @cImport({
+    @cInclude("pcap/pcap.h");
+});
+
+pub fn main() !void {
+    // stdout is for the actual output of your application, for example if you
+    // are implementing gzip, then only the compressed bytes should be sent to
+    // stdout, not dev debugging messages.
+    // const stdout_file = std.io.getStdOut().writer();
+    // var bw = std.io.bufferedWriter(stdout_file);
+    // const stdout = bw.writer();
+
+
+    // try bw.flush(); // don't forget to flush!
+
+    var pcap_err_buf: [c.PCAP_ERRBUF_SIZE]u8 = undefined;
+    const init_ret = c.pcap_init(c.PCAP_CHAR_ENC_UTF_8, &pcap_err_buf);
+    std.debug.print("init_ret: {d}\n", .{ init_ret });
+
+    var pcap_devs: ?*c.pcap_if_t = null;
+    const dev_ret = c.pcap_findalldevs(&pcap_devs, &pcap_err_buf);
+    std.debug.print("dev_et: {d}\n", .{ dev_ret });
+    defer c.pcap_freealldevs(pcap_devs);
+
+    std.debug.print("{any}\n", .{ pcap_devs });
+
+    if (pcap_devs) |dev| {
+        debugDev(dev);
+    } else {
+        std.debug.print("No devices\n", .{});
+        return;
+    }
+
+    const dev = c.pcap_create("any", &pcap_err_buf) orelse {
+        std.debug.print("No dev device: {s}\n", .{ pcap_err_buf });
+        return;
+    };
+    defer c.pcap_close(dev);
+
+    std.debug.print("Any: {any}\n", .{ dev });
+
+    var bpf: c.bpf_program = undefined;
+    _ = bpf;
+
+    // const rfmon_ret = c.pcap_set_rfmon(dev, 1);
+    // std.debug.print("rfmon_ret: {d}\n", .{ rfmon_ret });
+
+    // const promisc_ret = c.pcap_set_promisc(dev, 1);
+    // std.debug.print("promisc_ret: {d}\n", .{ promisc_ret });
+
+    const timeout_ret = c.pcap_set_timeout(dev, 200);
+    std.debug.print("timeout_ret: {d}\n", .{ timeout_ret });
+
+    // const snaplen_ret = c.pcap_set_snaplen(dev, 65535);
+    // std.debug.print("snapshot_ret: {d}\n", .{ snaplen_ret });
+
+    const activate_ret = c.pcap_activate(dev);
+    std.debug.print("activate_ret: {d}\n", .{ activate_ret });
+    if (activate_ret < 0) {
+        std.debug.print("Error: {s}\n", .{ c.pcap_geterr(dev) });
+        return;
+    }
+
+    const loop_ret = c.pcap_loop(dev, 0, callback, null);
+    std.debug.print("loop_ret: {d}\n", .{ loop_ret });
+}
+
+pub export fn callback(user: [*c]u8, header: [*c]const c.pcap_pkthdr, bytes: [*c]const u8) void {
+    _ = user;
+    std.debug.print("Callback called\n", .{});
+    std.debug.print("Header: {any}\n", .{ header.* });
+    const data = bytes[0..header.*.caplen];
+    std.debug.print("{x}\n", .{ std.fmt.fmtSliceHexLower(data) });
+    const eth = ether.Header.parse(data);
+    std.debug.print("Ethernet: {any}\n", .{ eth });
+    std.debug.print("  From: {x}:{x}:{x}:{x}:{x}:{x}\n", .{ eth.source[0], eth.source[1], eth.source[2], eth.source[3], eth.source[4], eth.source[5] });
+    std.debug.print("  To:   {x}:{x}:{x}:{x}:{x}:{x}\n", .{ eth.dest[0], eth.dest[1], eth.dest[2], eth.dest[3], eth.dest[4], eth.dest[5] });
+}
+
+pub fn debugDev(dev: *const c.pcap_if_t) void {
+    std.debug.print("Device: {s}\n", .{ dev.name });
+    std.debug.print("Flags: {d}\n", .{ dev.flags });
+    if (dev.description) |desc| {
+        std.debug.print("Description: {s}\n", .{ desc });
+    }
+    if (dev.next) |next| {
+        std.debug.print("-----\n", .{});
+        debugDev(next);
+    }
+}
+
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
