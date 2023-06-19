82e1ee6cc0535826f3dbdbb1605de4a29fab45ad
dantecatalfamo <dante.catalfamo@gmail.com>
2023-06-19 00:12:05 -0400

    Remove unnecessary casting

--- src/gui.zig
+++ src/gui.zig
@@ -34,8 +34,8 @@ pub fn runGui(gui_state: *root.GuiState) void {
         else
             gui_state.graph_bytes.slice();
 
-        const screen_width = @intCast(u64, c.GetScreenWidth());
-        const screen_height = @intCast(u64, c.GetScreenHeight());
+        const screen_width = c.GetScreenWidth();
+        const screen_height = c.GetScreenHeight();
         const x_over = @intCast(u64, @max(0, @intCast(i64, packet_slice.len) - @intCast(i64, screen_width)));
 
         const tallest_line = blk: {
@@ -49,7 +49,7 @@ pub fn runGui(gui_state: *root.GuiState) void {
         };
         const scale = @intToFloat(f64, tallest_line) / @intToFloat(f64, screen_height);
 
-        var x_line = @intCast(c_int, screen_width);
+        var x_line = screen_width;
         while (x_line > 0) : (x_line -= 60) {
             if (x_line == screen_width)
                 continue;
@@ -59,18 +59,18 @@ pub fn runGui(gui_state: *root.GuiState) void {
             }
         }
 
-        var y_dotted_line = @intCast(c_int, 3);
+        var y_dotted_line = @as(c_int, 3);
         while (y_dotted_line < screen_width) : (y_dotted_line += 10) {
-            c.DrawLine(y_dotted_line, @divTrunc(@intCast(c_int, screen_height), 2), y_dotted_line + 5, @divTrunc( @intCast(c_int, screen_height), 2), c.LIGHTGRAY);
+            c.DrawLine(y_dotted_line, @divTrunc(screen_height, 2), y_dotted_line + 5, @divTrunc(screen_height, 2), c.LIGHTGRAY);
         }
 
         for (packet_slice, 0..) |item, idx| {
             if (@intCast(isize, packet_slice.len)-@intCast(isize, idx) > c.GetScreenWidth())
                 continue;
-            const tcp_scaled = @floatToInt(u64, @intToFloat(f64, item.tcp) / scale);
-            const udp_scaled = @floatToInt(u64, @intToFloat(f64, item.udp) / scale);
+            const tcp_scaled = @floatToInt(c_int, @intToFloat(f64, item.tcp) / scale);
+            const udp_scaled = @floatToInt(c_int, @intToFloat(f64, item.udp) / scale);
 
-            const x_pos = screen_width-@min(screen_width, packet_slice.len)+(idx-x_over);
+            const x_pos = @intCast(u64, screen_width)-@min(@intCast(u64, screen_width), packet_slice.len)+(idx-x_over);
             const tcp_y_pos = screen_height-@min(tcp_scaled, screen_height);
             const udp_y_pos = screen_height-@min(udp_scaled+tcp_scaled, screen_height);
             c.DrawRectangle(
@@ -92,7 +92,7 @@ pub fn runGui(gui_state: *root.GuiState) void {
         if (c.IsCursorOnScreen()) {
             const mouse_x = c.GetMouseX();
 
-            const from_edge = @intCast(c_int, screen_width) - mouse_x + 1;
+            const from_edge = screen_width - mouse_x + 1;
             const slice_item = if (from_edge <= packet_slice.len)
                 packet_slice[packet_slice.len-@intCast(usize, from_edge)]
             else
@@ -113,7 +113,7 @@ pub fn runGui(gui_state: *root.GuiState) void {
             var rect = c.Rectangle{ .x = 0, .y = 0, .width = @intToFloat(f32, longest_toolip_text) + 6, .height = 35 };
             tooltipTransform(&rect);
 
-            c.DrawLine(mouse_x, 0, mouse_x, @intCast(c_int, screen_height), SkyTransparent);
+            c.DrawLine(mouse_x, 0, mouse_x, screen_height, SkyTransparent);
             _ = c.GuiPanel(rect, null);
             c.DrawText(c.TextFormat("Total: %d", slice_item.total()), @floatToInt(c_int, rect.x + 3), @floatToInt(c_int, rect.y + 3), 10, c.GRAY);
             c.DrawText(c.TextFormat("TCP: %d", slice_item.tcp), @floatToInt(c_int, rect.x + 3), @floatToInt(c_int, rect.y + 13), 10, c.GRAY);
@@ -122,9 +122,9 @@ pub fn runGui(gui_state: *root.GuiState) void {
 
         const showing_type = if (gui_state.packet_view) "Packets".ptr else "Bytes".ptr;
         const dev_name_width = c.MeasureText(dev_name, 10);
-        c.DrawText(dev_name, @divTrunc(@intCast(c_int, screen_width), 2) - @divTrunc(dev_name_width, 2), 5, 10, c.GRAY);
+        c.DrawText(dev_name, @divTrunc(screen_width, 2) - @divTrunc(dev_name_width, 2), 5, 10, c.GRAY);
         c.DrawText(c.TextFormat("%d %s", tallest_line, showing_type), 5, 5, 10, c.GRAY);
-        c.DrawText(c.TextFormat("%d", tallest_line / 2), 5, @divTrunc(@intCast(c_int, screen_height), 2) + 5, 10, c.GRAY);
+        c.DrawText(c.TextFormat("%d", tallest_line / 2), 5, @divTrunc(screen_height, 2) + 5, 10, c.GRAY);
         _ = c.GuiCheckBox(c.Rectangle{ .x = @intToFloat(f32, @max(80, screen_width) - 80), .y = 5, .width = 10, .height = 10 }, "Packet view", &gui_state.packet_view);
 
         c.EndDrawing();
